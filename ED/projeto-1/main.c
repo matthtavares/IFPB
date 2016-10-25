3a16bee86f1f91fcae0bba5d3a6b9b497cd1b99f
Mateus Tavares <mateus460@gmail.com>
2016-10-25 06:42:39 -0300

    Adicionado projeto de Estrutura de Dados

---
+++ ED/projeto-1/main.c
@@ -0,0 +1,78 @@
+#include "projeto.h"
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    lista lst,retorno;
+    no valor;
+    int posicao;
+    int *telefone;
+
+    //iniciando dados
+    strcpy(valor.nome,"Ronily segundo");
+    valor.telefone = 12345;
+    strcpy(valor.email,"teste@teste.com");
+    valor.dt_nascimento.dia = 1;
+    valor.dt_nascimento.mes = 2;
+    valor.dt_nascimento.ano = 2015;
+
+    //criando a lista
+    criar(&lst);
+
+    //inserindo 1º elemento na posicao 1
+    inserir(&lst,1,valor);
+
+    //iniciando dados do segundo elemento
+    strcpy(valor.nome,"Ronily primeiro");
+    valor.telefone = 123;
+    strcpy(valor.email,"teste@teste.com");
+    valor.dt_nascimento.dia = 1;
+    valor.dt_nascimento.mes = 2;
+    valor.dt_nascimento.ano = 2015;
+
+    //inserindo 2º elemento na primeira posicao
+    inserir(&lst,1,valor);
+
+    // apartir daqui caso queira ver funcionando alguns teste , eh so tirar o comentario - by Ronily
+
+    //elemento(lst,1,&retorno);
+    //printf("elemento.nome = %s\n", retorno->nome);
+
+    if(busca(lst,"Ronily primeiro")) printf("busca deu certo\n");
+    else printf("deu errado\n");
+
+    /*
+    strcpy(valor.nome,"Editado");
+    valor.telefone = 9999;
+    strcpy(valor.email,"editado@editado.com");
+    valor.dt_nascimento.dia = 999;
+    valor.dt_nascimento.mes = 999;
+    valor.dt_nascimento.ano = 999;
+    atualizar(&lst,1,valor);
+    */
+
+    printf("\n");
+
+    imprime(lst);
+
+    system("pause");
+
+    //printf("%d",busca(lst,"ROnily GOmes"));
+
+    //limpar(&lst);
+
+    //if(vazia(lst)) printf("vazia\n");
+
+    //printf("tamanho = %d\n", tamanho(lst));
+
+    //inserir(&lst,2,valor);
+    //inserir(&lst,3,valor);
+
+    //remover(&lst,2,telefone);
+    //printf("%d\n", *telefone);
+
+    printf("tamanho = %d\n", tamanho(lst));
+
+    return 0;
+}
---
+++ ED/projeto-1/projeto.h
@@ -0,0 +1,66 @@
+//definindo a estrutura de data
+typedef struct{
+    int dia;
+    int mes;
+    int ano;
+}data;
+
+// Definindo a estrutura de dados
+typedef struct stnolista {
+    char nome[20];
+    int telefone;
+    char email[20];
+    data dt_nascimento;
+    struct stnolista *prox; // campo do ponteiro para o proximo no
+}no;
+
+// Criando o tipo lista
+typedef no* lista;
+
+// Criando uma lista vazia
+void criar(lista *lst);
+
+// Verificar se a lista esta vazia
+int vazia(lista lst);
+
+// Obter o tamanho da lista
+int tamanho(lista lst);
+
+// Obter o valor do elemento de uma determinada posicao
+int elemento (lista lst, int posicao, lista *retorno);
+
+// Obter a posicao do elemento, cujo valor(nome) eh fornecido
+int busca(lista lst, char *nome);
+
+// Listar dados de uma determinada posicao
+int listar(lista lst, int posicao);
+
+// inserir um elemento na lista, dada a sua posicao
+int inserir( lista *lst, int posicao, no valor);
+
+// atualizar um elemento na lista, dada a sua posicao
+int atualizar( lista *lst, int posicao, no valor);
+
+// Remover um elemento de uma determinada posicao
+int remover(lista *lst, int posicao, int *telefone);
+
+// Remover todos os elementos da lista
+int limpar(lista *lst);
+
+// Exibe todos os elementos da lista
+void imprime (lista lst);
+
+// valida o nome
+int validaNome(char *nome);
+
+// valida o email
+int validaEmail(char *email);
+
+// valida o telefone
+int validaTelefone(int telefone);
+
+// valida a data
+int validaData(data d);
+
+// verifica se ja existe o telefone
+int verificaTelefone(lista lst, int tel);
