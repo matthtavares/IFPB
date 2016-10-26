fc451264c4fc104b21806cb4e6bcace90638a690
Mateus Tavares <mateus460@gmail.com>
2016-10-25 21:58:03 -0300

    Atualizacoes no main.c e agenda.c

--- ED/projeto-1/main.c
+++ ED/projeto-1/main.c
@@ -1,82 +1,113 @@
 #include "agenda.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main()
 {
     lista lst,retorno;
     no valor;
-    int posicao;
+    int posicao,ctrl = 1,acao;
     int *telefone;
-
-    //iniciando dados
-    strcpy(valor.nome,"Ronily segundo");
-    valor.telefone = 12345;
-    strcpy(valor.email,"teste@teste.com");
-    valor.dt_nascimento.dia = 1;
-    valor.dt_nascimento.mes = 2;
-    valor.dt_nascimento.ano = 2015;
+    char nome[20];
 
     //criando a lista
     criar(&lst);
 
-    //inserindo 1º elemento na posicao 1
-    inserir(&lst,1,valor);
+    while(ctrl)
+    {
+        menu();
 
-    //iniciando dados do segundo elemento
-    strcpy(valor.nome,"Ronily primeiro");
-    valor.telefone = 123456;
-    strcpy(valor.email,"teste@teste.com");
-    valor.dt_nascimento.dia = 1;
-    valor.dt_nascimento.mes = 2;
-    valor.dt_nascimento.ano = 2015;
+        printf("- Informe sua acao: ");
+        scanf("%d%*c",&acao);
 
-    //inserindo 2º elemento na primeira posicao
-    //if( !inserir(&lst,1,valor) ){ printf("Telefone ja cadastrado!\n"); }
+        switch(acao)
+        {
+            case 1: 
+                printf("\n## INFORMACOES DO CONTATO: \n\n");
+                printf("Posicao: ");
+                scanf("%d%*c",&posicao);
+                printf("Nome: ");
+                scanf("%[^\n]s",valor.nome);
+                printf("Telefone: ");
+                scanf("%*c%d",&valor.telefone);
+                printf("Email [seunome@email.com]: ");
+                scanf("%s",valor.email);
+                printf("Data Nasc. [dd/mm/aaaa]: ");
+                scanf("%d/%d/%d%*c",&valor.dt_nascimento.dia,&valor.dt_nascimento.mes,&valor.dt_nascimento.ano);
 
-    // apartir daqui caso queira ver funcionando alguns teste , eh so tirar o comentario - by Ronily
+                if(inserir(&lst,posicao,valor))
+                    printf("\n>> Contato inserido com sucesso! <<\n\n");
+                else 
+                    printf("\n>> Ops, nao foi possivel inserir o contato! <<\n\n");
+                break;
 
-    //elemento(lst,1,&retorno);
-    //printf("elemento.nome = %s\n", retorno->nome);
+            case 2:
+                printf("\n## INFORMACOES DO CONTATO: \n\n");
+                printf("Posicao: ");
+                scanf("%d%*c",&posicao);
+                printf("Nome: ");
+                scanf("%[^\n]s",valor.nome);
+                printf("Telefone: ");
+                scanf("%*c%d",&valor.telefone);
+                printf("Email: (seunome@email.com)");
+                scanf("%s",valor.email);
+                printf("Data Nasc. [dd/mm/aaaa]: ");
+                scanf("%d/%d/%d%*c",&valor.dt_nascimento.dia,&valor.dt_nascimento.mes,&valor.dt_nascimento.ano);
 
-    //if(busca(lst,"Ronily primeiro")) printf("busca deu certo\n");
-    //else printf("deu errado\n");
+                if(atualizar(&lst,posicao,valor))
+                    printf("\n>> Contato atualizado com sucesso! <<\n\n");
+                else 
+                    printf("\n>> Ops, nao foi possivel atualizar o contato! <<\n\n");
+                break;
 
-    /*
-    strcpy(valor.nome,"Editado");
-    valor.telefone = 9999;
-    strcpy(valor.email,"editado@editado.com");
-    valor.dt_nascimento.dia = 999;
-    valor.dt_nascimento.mes = 999;
-    valor.dt_nascimento.ano = 999;
-    atualizar(&lst,1,valor);
-    */
+            case 3: 
+                printf("\n## CONTATO QUE SERA EXCLUIDO: \n\n");
+                printf("Posicao: ");
+                scanf("%d%*c",&posicao);
 
-    printf("\n\n");
+                if(remover(&lst,posicao,telefone))
+                    printf("\n>> Contato removido com sucesso! <<\n\n");
+                else 
+                    printf("\n>> Ops, nao foi possivel remover o contato! <<\n\n");
+                break;
 
-    //menu();
+            case 4: 
+                printf("\n## CONTATO QUE SERA PESQUISADO: \n\n");
+                printf("Nome: ");
+                scanf("%[^\n]s",nome);
+                getchar();
 
-    printf("\n\n");
+                if(busca(lst,nome))
+                    printf("\n>> Contato existe! <<\n\n");
+                else 
+                    printf("\n>> Ops, nao foi possivel achar o contato! <<\n\n");
+                break;
 
-    imprime(lst);
+            case 5:
+                printf("\n## TODOS OS CONTATOS: \n\n");
+                imprime(lst);
+                break;
 
-    //printf("%d",busca(lst,"ROnily GOmes"));
+            case 6:
+                limpar(&lst);
+                printf("\n>> Todos os contatos foram excluidos! <<\n\n");
+                break;
 
-    //limpar(&lst);
+            case 7:
+                if(vazia(lst))
+                    ctrl = 0;
+                else   
+                    printf("\n>> Ainda ha registro(s) na agenda, apague antes de sair! <<\n\n"); 
+                break;
 
-    //printf("%d vazia\n", vazia(lst));
+            default: 
+                printf("\n>> Acao invalida! <<\n\n");
+        }
+        system("pause");
+        system("cls");
+    }
 
-    //printf("tamanho = %d\n", tamanho(lst));
-
-    //inserir(&lst,2,valor);
-    //inserir(&lst,3,valor);
-
-    //remover(&lst,2,telefone);
-    //printf("%d\n", *telefone);
-
-    //printf("tamanho = %d\n", tamanho(lst));
-
-    system("pause");
 
     return 0;
 }
